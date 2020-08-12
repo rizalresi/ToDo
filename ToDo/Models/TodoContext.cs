7c2937dc062f1b61c482e83794ff672b3c08863e
rizalresi <rizal.resi@gmail.com>
2020-08-12 15:18:11 +0700

    initial commit

---
+++ ToDo/Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> ToDos { get; set; }
+    }
+}
---
+++ ToDo/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Complete { get; set; }
+    }
+}
